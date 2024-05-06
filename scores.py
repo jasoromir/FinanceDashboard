798c7cad4ada740bfaee79b199e8afb52a06b4bc
Jason <jason.omedes@gmail.com>
2024-05-06 12:07:40 +0200

    Add scores to tickers file

---
+++ scores.py
@@ -0,0 +1,17 @@
+
+scores = {
+    "eps": 3,
+    "pe": 3,
+    "dcf10": 3,
+    "roce": 3,
+    "roe": 2,
+    "sgr": 2,
+    "free_cash_flow": 2,
+    "peg": 2,
+    "earning_yield": 1,
+    "munger_multiplier": 1,
+    "buffet_ratio": 1,
+    "ncav": 1,
+    "dygr":1,
+    "balance_strength": 0,
+}
